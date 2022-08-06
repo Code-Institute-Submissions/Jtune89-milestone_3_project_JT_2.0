509b15df77a727420e6be8003815bd3df9609656
Jtune89 <Joseph.tune1@gmail.com>
2022-08-06 13:45:37 +0000

    user information input start

--- run.py
+++ run.py
@@ -12,6 +12,14 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Quiz Results')
 
+
+# Welcome message to Quiz
+print("Welcome to the most AMAZING quiz that ever was or will be \n")
+
+#user_information
+input ("What is your name? \nName: ")
+print ("\nWelcome (name) & good luck!  Lets start with question 1:")
+
 # QUESTION 1
-input ("What is the capital of India? \n a. New Delhi \n b. Mumbai \n c. Jaipur \n d. Kolkata \n Answer: ")
+input ("\n What is the capital of India? \n a. New Delhi \n b. Mumbai \n c. Jaipur \n d. Kolkata \n Answer: ")
 print (input)
